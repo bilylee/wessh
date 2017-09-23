5837ca4a868b80629120fb37041cb50f48797d75
bily lee <bily.lee@qq.com>
2017-09-23 22:09:05 +0800

    :tada: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # wessh
-Make your wechat function like ssh
+Make your WeChat function like ssh.
+
+## Introduction
+Use this simple python script, you can:
+ - send commands to server by WeChat just like what you will do in a ssh session.
+ - download files to WeChat from server.
+ - get log files of executed commands.
+
+## Dependency
+- itchat
+
+## Usage
+1. clone this repository to your server
+```bash
+git clone https://github.com/bilylee/wessh.git
+```
+
+2. run this script in your server
+```bash
+cd wessh
+tmux # put commands that last for a long time in tmux :)
+python wessh.py
+```
+
+3. scan the QR code to login your wechat account.
+
+4. now you can send bash commands in your filehelper just like in a ssh.
+![demo image](demo.PNG)
+**CAUTION**: you can only send commands by your phone since WeChat doesn't allow multiple logins in different devices. Use other devices(like computer or web browsers), this script will exit. 
+
+## Advanced usage
+- you can get help message by using the `help` command.
+- commands that doesn't belong to the `SIMPLE_OUTPUT_CMD` list will save their stdout and stderr to a log file. You can get this log file by use `log` command if this is the lastest command, or `log index_number` where index_number is the interger number right below your sent command.
+- use `dl filepath` to download files from your server. 
+
+
+
---
+++ wessh.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2017 bily     Huazhong University of Science and Technology
+#
+# Distributed under terms of the MIT license.
+
+import itchat
+from subprocess import Popen, check_output
+
+# commands that you'd like to immediately get log message.
+SIMPLE_OUTPUT_CMD = ['ls', 'pwd', 'whoami', 'users', 'git', 'cat', 'nvidia-smi', 'df']
+FORBIDDEN_CMD = ['vim', 'top', 'htop', 'ipython']
+FREQUENT_CMD = ['nvidia-smi']
+
+def send_file(filepath):
+  """send file to your wechat filehelper
+
+  :param filepath: path of the file to be sent, can be relative/absolute path
+  :return: None
+  """
+  status = itchat.send('@fil@{}'.format(filepath), toUserName='filehelper')
+  return status
+
+def send_msg(msg):
+  """send message to your wechat filehelper
+
+  :param msg: message of string type
+  :return: None
+  """
+  itchat.send(msg, toUserName='filehelper')
+
+def get_log_file_path(ind=None, create_new=True, log_prefix='/tmp/welog', maximum_log_file=100):
+  """get log file path
+
+  :param ind: log file index
+  :param create_new: if create new log file
+  :param log_prefix: prefix of log file
+  :param maximum_log_file: maximum number of log files
+  :return: log file index, log file path
+  """
+  cur_ind = get_log_file_path.cur_ind
+  if create_new:
+    ind = (cur_ind + 1) % maximum_log_file
+    get_log_file_path.cur_ind = ind
+  else:
+    if ind is None:
+      ind = cur_ind
+    ind = ind % maximum_log_file
+  return ind, '{}.{}.txt'.format(log_prefix, ind)
+get_log_file_path.cur_ind = 0
+
+@itchat.msg_register([itchat.content.TEXT])
+def chat_trigger(msg):
+  try:
+    msg_text = msg['Text'].strip()
+    cmd = msg_text.split(' ')
+
+    if cmd[0] == 'help':
+      send_msg('Send command just like in ssh.\n'
+               'additional useful commands:\n'
+               'log [index]: download the log file of the index-th command\n'
+               'dl filepath: download file\n'
+               'list: list frequently used commands\n')
+    elif cmd[0] == 'list':
+      send_msg('\n'.join(FREQUENT_CMD))
+    elif cmd[0] == 'dl':
+      if len(cmd) == 2:
+        send_file(cmd[1])
+      else:
+        send_msg('usage: dl filepath')
+    elif cmd[0] == 'log':
+      if len(cmd) == 2:
+        ind = int(cmd[1])
+      else:
+        ind = None
+      ind, log_file = get_log_file_path(ind=ind, create_new=False)
+      send_file(log_file)
+    elif cmd[0] in SIMPLE_OUTPUT_CMD:
+      out = check_output(cmd)
+      send_msg(out)
+    elif cmd[0] in FORBIDDEN_CMD:
+      send_msg('{} are not supported...'.format(','.join(FORBIDDEN_CMD)))
+    else:
+      ind, log_file = get_log_file_path()
+      with open(log_file, 'w') as f:
+        Popen(cmd, stdout=f, stderr=f)
+      send_msg('log index: {}'.format(ind))
+
+  except Exception as e:
+    send_msg(e.__str__())
+
+if __name__ == '__main__':
+  itchat.auto_login(hotReload=True, enableCmdQR=2)
+  itchat.run()
